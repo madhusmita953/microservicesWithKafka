1b928dc8f54d411ea73ca00463be0f483a52baa0
madhusmita953 <mca.madhu87@gmail.com>
2026-05-30 23:52:49 +0530

    feat: Add ApiGatewayApplication

---
+++ api-gateway/src/main/java/com/ecommerce/microservices/gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.ecommerce.microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * API Gateway using Spring Cloud Gateway
+ * Routes requests to microservices registered with Eureka
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
