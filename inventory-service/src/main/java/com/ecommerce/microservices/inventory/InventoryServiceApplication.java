a82c776af63f8bf66bf56318404adc88674c0e3c
madhusmita953 <mca.madhu87@gmail.com>
2026-05-31 06:17:11 +0530

    feat: Add InventoryServiceApplication

---
+++ inventory-service/src/main/java/com/ecommerce/microservices/inventory/InventoryServiceApplication.java
@@ -0,0 +1,17 @@
+package com.ecommerce.microservices.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Inventory Microservice - Handles inventory management
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+}
